c453ac2136f3b75603817e224903299cb0861725
LupinaIgor <igorlupina222@gmail.com>
2024-09-14 11:52:20 +0300

    Clothes added

--- WebApplicationDolly/WebApplicationDolly/obj/Debug/net8.0/WebApplicationDolly.AssemblyInfo.cs
+++ WebApplicationDolly/WebApplicationDolly/obj/Debug/net8.0/WebApplicationDolly.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplicationDolly")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+300c652768aaa2d1ea8e895b4e4a49b517b8a4f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd95305b21d4e0717177527281d5308a4b1255bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplicationDolly")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplicationDolly")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
